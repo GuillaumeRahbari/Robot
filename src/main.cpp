ad233cfeed842d106c710e42ebdfc9a4f3c9e192
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2014-11-15 10:51:13 +0100

    Base du code

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Robot
+//
+//  Created by Guillaume Rahbari on 15/11/2014.
+//  Copyright (c) 2014 Guillaume Rahbari. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
