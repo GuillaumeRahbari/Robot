87d134dcce182be04be13c41f41b4d69006af21b
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-01-03 14:54:23 +0100

    Mise à jour de EvaluerPlot

--- src/CommandeEvaluerPlot.cpp
+++ src/CommandeEvaluerPlot.cpp
@@ -1,1 +1,13 @@
 #include "CommandeEvaluerPlot.h"
+
+CommandeEvaluerPlot CommandeEvaluerPlot::monExemplaire("EVALUERPLOT");
+
+Commande* CommandeEvaluerPlot::constructeur(Robot* r, Invocateur* inv) {
+	Commande* cmd = new CommandeEvaluerPlot("EVALUERPLOT", r);
+	Commande::cmdExecutees.push_back(cmd);
+    return cmd;
+}
+
+void CommandeEvaluerPlot::execute() {
+    robot->evaluerPlot();
+}
--- src/CommandeEvaluerPlot.h
+++ src/CommandeEvaluerPlot.h
@@ -1,7 +1,22 @@
 #ifndef Commande_EvaluerPlot_h
 #define Commande_EvaluerPlot_h
 
-// #include <stddef.h>
-// #include <iostream>
+#include "CommandeRobot.h"
+
+class CommandeEvaluerPlot : public CommandeRobot{
+private:
+	Objet o;
+
+public:
+	static CommandeEvaluerPlot monExemplaire;
+
+	CommandeEvaluerPlot(string nom) : CommandeRobot(nom) {};
+
+	CommandeEvaluerPlot(string nom, Robot* r) : CommandeRobot(nom, r) {};
+
+	virtual Commande* constructeur(Robot* r, Invocateur* inv);
+
+	virtual void execute();
+};
 
 #endif
