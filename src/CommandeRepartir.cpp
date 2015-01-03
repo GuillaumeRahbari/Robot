cf65e6bceb76dbc6e3cd20f9d9fe98de0dab08c5
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-01-03 15:20:07 +0100

    Mise à jour de la commande repartir.

--- src/CommandeRepartir.cpp
+++ src/CommandeRepartir.cpp
@@ -1,1 +1,17 @@
 #include "CommandeRepartir.h"
+
+CommandeRepartir CommandeRepartir::monExemplaire("REPARTIR");
+
+Commande* CommandeRepartir::constructeur(Robot* r, Invocateur* inv) {
+	Commande* cmd = new CommandeRepartir("REPARTIR", r);
+	Commande::cmdExecutees.push_back(cmd);
+    return cmd;
+}
+
+void CommandeRepartir::execute() {
+    robot->repartir();
+}
+
+void CommandeRepartir::desexecute() {
+    robot->figer();
+}
--- src/CommandeRepartir.h
+++ src/CommandeRepartir.h
@@ -1,7 +1,21 @@
 #ifndef Commande_Repartir_h
 #define Commande_Repartir_h
 
-// #include <stddef.h>
-// #include <iostream>
+#include "CommandeRobot.h"
+
+class CommandeRepartir : public CommandeRobot{
+public:
+	static CommandeRepartir monExemplaire;
+
+	CommandeRepartir(string nom) : CommandeRobot(nom) {};
+
+	CommandeRepartir(string nom, Robot* r) : CommandeRobot(nom, r) {};
+
+	virtual Commande* constructeur(Robot* r, Invocateur* inv);
+
+	virtual void execute();
+
+	virtual void desexecute();
+};
 
 #endif
