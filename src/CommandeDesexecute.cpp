72286dc851f30c9217025885fb5b5b4afc707906
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-01-03 13:52:05 +0100

    Classe Desexecute.

--- src/CommandeDesexecute.cpp
+++ src/CommandeDesexecute.cpp
@@ -1,2 +1,15 @@
 #include "CommandeDesexecute.h"
 
+CommandeDesexecute CommandeDesexecute::monExemplaire("DESEXECUTE");
+
+Commande* CommandeDesexecute::constructeur(Robot* r, Invocateur* inv) {
+    return new CommandeDesexecute("DESEXECUTE");
+}
+
+void CommandeDesexecute::execute() {
+    Commande::cmdExecutees[Commande::cmdExecutees.size()-1]->desexecute();
+}
+
+void CommandeDesexecute::desexecute() {
+    ;
+}
--- src/CommandeDesexecute.h
+++ src/CommandeDesexecute.h
@@ -1,17 +1,22 @@
 #ifndef Commande_Desexecute_h
 #define Commande_Desexecute_h
 
-// #include <stddef.h>
-// #include <iostream>
 #include <string>
 #include "Commande.h"
 using namespace std;
 
 class CommandeDesexecute : public Commande{
 public:
+	static CommandeDesexecute monExemplaire;
 
+	CommandeDesexecute(string nom) : Commande(nom) {};
+
+	virtual Commande* constructeur(Robot* r, Invocateur* inv);
+
+	virtual void execute();
+
+	virtual void desexecute();
 	
 };
-
 
 #endif
