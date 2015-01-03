89377e9e1c5408fe0ef6ee9f2141f417e843e1c6
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-01-03 15:13:55 +0100

    Mise à jour de la commande rencontrer plot.

--- src/CommandeRencontrerPlot.cpp
+++ src/CommandeRencontrerPlot.cpp
@@ -1,1 +1,13 @@
 #include "CommandeRencontrerPlot.h"
+
+CommandeRencontrerPlot CommandeRencontrerPlot::monExemplaire("RENCONTRERPLOT");
+
+Commande* CommandeRencontrerPlot::constructeur(Robot* r, Invocateur* inv) {
+	Commande* cmd = new CommandeRencontrerPlot("RENCONTRERPLOT", r, inv->getInt());
+	Commande::cmdExecutees.push_back(cmd);
+    return cmd;
+}
+
+void CommandeRencontrerPlot::execute() {
+    robot->rencontrerPlot(p);
+}
--- src/CommandeRencontrerPlot.h
+++ src/CommandeRencontrerPlot.h
@@ -1,7 +1,22 @@
 #ifndef Commande_RencontrerPlot_h
 #define Commande_RencontrerPlot_h
 
-// #include <stddef.h>
-// #include <iostream>
+#include "CommandeRobot.h"
+
+class CommandeRencontrerPlot : public CommandeRobot{
+private:
+	Plot p;
+
+public:
+	static CommandeRencontrerPlot monExemplaire;
+
+	CommandeRencontrerPlot(string nom) : CommandeRobot(nom) {};
+
+	CommandeRencontrerPlot(string nom, Robot* r, int hauteur) : CommandeRobot(nom, r), p(hauteur) {};
+
+	virtual Commande* constructeur(Robot* r, Invocateur* inv);
+
+	virtual void execute();
+};
 
 #endif
