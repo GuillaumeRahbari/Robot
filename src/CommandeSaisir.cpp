c1d65e926f2f21444153704557827711113e3aee
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-01-03 13:51:51 +0100

    Ajout du push_back ici.

--- src/CommandeSaisir.cpp
+++ src/CommandeSaisir.cpp
@@ -3,7 +3,9 @@
 CommandeSaisir CommandeSaisir::monExemplaire("SAISIR");
 
 Commande* CommandeSaisir::constructeur(Robot* r, Invocateur* inv) {
-    return new CommandeSaisir("SAISIR", r, inv->getInt());
+	Commande* cmd = new CommandeSaisir("SAISIR", r, inv->getInt());
+	Commande::cmdExecutees.push_back(cmd);
+    return cmd;
 }
 
 void CommandeSaisir::execute() {
