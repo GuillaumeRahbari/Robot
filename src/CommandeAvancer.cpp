7111486b70e7c70eda36c39a48e406198a40bf8b
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-01-03 14:54:12 +0100

    Mise à jour de CommandeAvancer.

--- src/CommandeAvancer.cpp
+++ src/CommandeAvancer.cpp
@@ -1,1 +1,18 @@
 #include "CommandeAvancer.h"
+
+CommandeAvancer CommandeAvancer::monExemplaire("AVANCER");
+
+Commande* CommandeAvancer::constructeur(Robot* r, Invocateur* inv) {
+	Commande* cmd = new CommandeAvancer("AVANCER", r, inv->getInt(), inv->getInt());
+	Commande::cmdExecutees.push_back(cmd);
+    return cmd;
+}
+
+void CommandeAvancer::execute() {
+	oldPos = robot->getPos();
+    robot->avancer(pos);
+}
+
+void CommandeAvancer::desexecute() {
+    robot->avancer(oldPos);
+}
--- src/CommandeAvancer.h
+++ src/CommandeAvancer.h
@@ -1,7 +1,27 @@
 #ifndef Commande_Avancer_h
 #define Commande_Avancer_h
 
-// #include <stddef.h>
-// #include <iostream>
+#include "CommandeRobot.h"
+#include "Position.h"
+
+class CommandeAvancer : public CommandeRobot{
+private:
+	Position pos;
+
+	Position oldPos;
+
+public:
+	static CommandeAvancer monExemplaire;
+
+	CommandeAvancer(string nom) : CommandeRobot(nom) {};
+
+	CommandeAvancer(string nom, Robot* r, int x, int y) : CommandeRobot(nom, r), pos(x,y) {};
+
+	virtual Commande* constructeur(Robot* r, Invocateur* inv);
+
+	virtual void execute();
+
+	virtual void desexecute();
+};
 
 #endif
