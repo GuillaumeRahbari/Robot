4e6222d83e38e9684fd51a38a14b863e0c25f146
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-01-03 15:07:14 +0100

    Mise à jour de la commande poser.

--- src/CommandePoser.cpp
+++ src/CommandePoser.cpp
@@ -1,1 +1,18 @@
 #include "CommandePoser.h"
+
+CommandePoser CommandePoser::monExemplaire("POSER");
+
+Commande* CommandePoser::constructeur(Robot* r, Invocateur* inv) {
+	Commande* cmd = new CommandePoser("POSER", r);
+	Commande::cmdExecutees.push_back(cmd);
+    return cmd;
+}
+
+void CommandePoser::execute() {
+	o = robot->getObjet();
+    robot->poser();
+}
+
+void CommandePoser::desexecute() {
+    robot->saisir(o);
+}
--- src/CommandePoser.h
+++ src/CommandePoser.h
@@ -1,7 +1,24 @@
 #ifndef Commande_Poser_h
 #define Commande_Poser_h
 
-// #include <stddef.h>
-// #include <iostream>
+#include "CommandeRobot.h"
+
+class CommandePoser : public CommandeRobot{
+private:
+	Objet o;
+
+public:
+	static CommandePoser monExemplaire;
+
+	CommandePoser(string nom) : CommandeRobot(nom) {};
+
+	CommandePoser(string nom, Robot* r) : CommandeRobot(nom, r) {};
+
+	virtual Commande* constructeur(Robot* r, Invocateur* inv);
+
+	virtual void execute();
+
+	virtual void desexecute();
+};
 
 #endif
