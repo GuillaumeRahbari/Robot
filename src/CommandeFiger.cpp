10e790f6ea5e2dc40215600a78d6d5adf8b6f070
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-01-03 14:58:03 +0100

    Mise à jour de commande Figer.

--- src/CommandeFiger.cpp
+++ src/CommandeFiger.cpp
@@ -1,1 +1,17 @@
 #include "CommandeFiger.h"
+
+CommandeFiger CommandeFiger::monExemplaire("FIGER");
+
+Commande* CommandeFiger::constructeur(Robot* r, Invocateur* inv) {
+	Commande* cmd = new CommandeFiger("FIGER", r);
+	Commande::cmdExecutees.push_back(cmd);
+    return cmd;
+}
+
+void CommandeFiger::execute() {
+    robot->figer();
+}
+
+void CommandeFiger::desexecute() {
+    robot->repartir();
+}
--- src/CommandeFiger.h
+++ src/CommandeFiger.h
@@ -1,7 +1,20 @@
 #ifndef Commande_Figer_h
 #define Commande_Figer_h
 
-// #include <stddef.h>
-// #include <iostream>
+#include "CommandeRobot.h"
 
+class CommandeFiger : public CommandeRobot{
+public:
+	static CommandeFiger monExemplaire;
+
+	CommandeFiger(string nom) : CommandeRobot(nom) {};
+
+	CommandeFiger(string nom, Robot* r) : CommandeRobot(nom, r) {};
+
+	virtual Commande* constructeur(Robot* r, Invocateur* inv);
+
+	virtual void execute();
+
+	virtual void desexecute();
+};
 #endif
