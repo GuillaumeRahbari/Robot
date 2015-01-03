9d677f09f47e38d6c1988e1766f75c9851e522a0
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-01-03 15:36:44 +0100

    Mise à jour de la commande Tourner.

--- src/CommandeTourner.cpp
+++ src/CommandeTourner.cpp
@@ -1,1 +1,18 @@
 #include "CommandeTourner.h"
+
+CommandeTourner CommandeTourner::monExemplaire("TOURNER");
+
+Commande* CommandeTourner::constructeur(Robot* r, Invocateur* inv) {
+	Commande* cmd = new CommandeTourner("TOURNER", r, inv->getString());
+	Commande::cmdExecutees.push_back(cmd);
+    return cmd;
+}
+
+void CommandeTourner::execute() {
+	oldDir = robot->getDir();
+    robot->tourner(dir);
+}
+
+void CommandeTourner::desexecute() {
+    robot->tourner(oldDir);
+}
--- src/CommandeTourner.h
+++ src/CommandeTourner.h
@@ -1,7 +1,26 @@
 #ifndef Commande_Tourner_h
 #define Commande_Tourner_h
 
-// #include <stddef.h>
-// #include <iostream>
+#include "CommandeRobot.h"
+
+class CommandeTourner : public CommandeRobot{
+private:
+	string dir;
+
+	string oldDir;
+
+public:
+	static CommandeTourner monExemplaire;
+
+	CommandeTourner(string nom) : CommandeRobot(nom) {};
+
+	CommandeTourner(string nom, Robot* r, string dir) : CommandeRobot(nom, r), dir(dir) {};
+
+	virtual Commande* constructeur(Robot* r, Invocateur* inv);
+
+	virtual void execute();
+
+	virtual void desexecute();
+};
 
 #endif
