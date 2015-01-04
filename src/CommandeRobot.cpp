2c25673924ce7d76c0fc220182e14eafac8a1374
Guillaume Rahbari <guillaume.rahbari@gmail.com>
2015-01-04 11:06:53 +0100

    Suppression des trucs inutiles.

--- src/CommandeRobot.cpp
+++ src/CommandeRobot.cpp
@@ -1,10 +1,1 @@
 #include "CommandeRobot.h"
-
-Commande* CommandeRobot::constructeur(Robot*, Invocateur* inv){
-	throw CommandeRobot::Bad_Commande();
-}
-
-void CommandeRobot::execute(){
-	cout << "je suis dans CommandeRobot" << endl;
-	// throw Commande::Bad_Commande();
-}
